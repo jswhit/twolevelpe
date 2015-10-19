0cca0c660b58c9bb422ede7bb017ebb5a2b651e4
Jeff Whitaker <jswhit@fastmail.fm>
2015-10-19 16:33:11 -0600

    update

--- compute_means.py
+++ compute_means.py
@@ -1,10 +1,14 @@
 import sys, numpy
-data = numpy.loadtxt(sys.argv[1])
+data = numpy.genfromtxt(sys.argv[1],invalid_raise=False)
 nskip = int(sys.argv[2])
-data = data[nskip:]
+if len(sys.argv) > 3:
+    nmax = int(sys.argv[3])+1
+else:
+    nmax = data.shape[0]
+data = data[nskip:nmax]
 ntimes = data[-1,0]-data[0,0]
 out = data[:,1:].mean(axis=0)
-strout = '%s ' % (ntimes,)
+strout = '%s ' % (int(ntimes),)
 for item in out:
     strout += '%6.4f ' % (item,)
 print strout
